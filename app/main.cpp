64686309cd67670ed1dd61c3ad11493129369d6e
Serjuice <mr.sm2002@mail.ru>
2024-06-30 17:56:13 +0500

    Delete some code

--- app/main.cpp
+++ app/main.cpp
@@ -3,11 +3,9 @@
 
 int main(int argc, char* argv[]) {
 
-    std::cout << "1 - Hello, World!" << std:: endl;
     std::cout << "2 - Hello, World!" << std:: endl;
     std::cout << "3 - Hello, World!" << std:: endl;
     std::cout << "4 - Hello, World!" << std:: endl;
-    // @Serjuice3
 
     return 0;
 }
